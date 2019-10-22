0ad61572ba512899a8db0e6fb40414b3c9fdda3f
Yuan Yuchuan <yyc478@gmail.com>
2019-10-22 00:13:53 -0700

    wip v1 working router

--- src/router.js
+++ src/router.js
@@ -1,19 +1,42 @@
 // @flow
+import { Plugin, PluginConfig } from "./plugin";
+
+const routerLocalStorageKey = "";
 
 export default class Router {
   // This class abstracts the fast storage of commands in localStorage
+  // And also does the command routing
 
-  constructor() {}
+  config: Object;
+
+  constructor() {
+    let confString = localStorage.getItem(routerLocalStorageKey);
+    if (!confString) {
+      this.config = {
+        commandSearches: [],
+        regexSearches: []
+      };
+    } else {
+      (confString: string);
+      this.config = JSON.parse(confString);
+    }
+  }
 
   registerCommand(key: string, state: ?any): void {}
 
   registerRegex(regex: string, state: ?any): void {}
 
   getSearchForQuery(query: string): string {
+    // Tries to find a matching command
+
+    // Failing that, tries all regex matches
+
     return "";
   }
 
   getSuggestForQuery(query: string): Array<[string, string]> {
     return [];
   }
+
+  *getMatchingCommandsForQuery(query: string): Iterable<Plugin<PluginConfig>> {}
 }
--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,8 @@
-import SearchPlugin from "./plugins/searchplugin";
+// @flow
 import SearchEngine from "./searchEngine";
+import { finished } from "stream";
+
+let se = new SearchEngine();
 
 self.addEventListener("install", event => {
   console.log("hi! Service worker is registered");
@@ -13,18 +16,36 @@ self.addEventListener("fetch", event => {
   console.log("you tried to request for ", event.request);
   let url = new URL(event.request.url);
   console.log(url.pathname);
-  if (url.pathname != "/find") {
+  if (url.pathname != "/find" || url.pathname != "/suggest") {
     console.log("wha");
     return fetch(event.request.url);
   }
   let query = url.searchParams.get("q");
   console.log(query);
+
+  if (url.pathname == "/suggest") {
+    suggest(event, query);
+  }
+  if (url.pathname == "/find") {
+    find(event, query);
+  }
+});
+
+function suggest(event, query: ?string): void {}
+function find(event, query: ?string): void {
   if (query == null) {
     event.respondWithText(
       event,
       "Hello from Janet, your plastic pal who's fun to be with"
     );
+    return;
   }
+
+  (query: string);
+
+  let url = se.router.getSearchForQuery(query);
+  event.respondWith(Response.redirect(url, 302));
+
   console.log("redirecting to google");
   event.respondWith(
     Response.redirect(
@@ -32,21 +53,11 @@ self.addEventListener("fetch", event => {
       302
     )
   );
-});
+}
 
 function respondWithText(event, txt) {
   let resp = new Response(txt, { headers: { "Content-Type": "text/html" } });
   event.respondWith(resp);
-}
-
-// probably still need some way to easily add plugins (so people can write their own custom plugins)
-
-let regex_url = RegExp("s|((.+))|(http[s]://.+$1)");
-function search(store, captures) {
-  if (typeof store == "string") {
-    // If URL is a regex replacement, do the replacement
-    // interpolate url if necessary
-  }
 }
 
 export default self;
